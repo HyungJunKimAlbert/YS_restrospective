6bc1e18e54d7cbf6ad1181ecb04479e11eb007b2
HyungjunKim <hjkim1568@naver.com>
2022-02-22 14:00:42 +0900

    Update Cr_baseline.py

--- Cr_baseline.py
+++ Cr_baseline.py
@@ -63,32 +63,66 @@ icuinfo_df = pd.read_csv(data_dir + 'icu_info.csv', encoding='cp949', index_col=
 '''    Urine output Criteria    ''' 
 
 # Urine output
-uo = data_df[data_df['item']=='Urineoutput'].iloc[:, :-1].sort_values(by=['pat_id', 'charttime']).reset_index(drop=True)
-uo = uo.rename(columns={'value':'uo'})
+# uo = data_df[data_df['item']=='Urineoutput'].iloc[:, :-1].sort_values(by=['pat_id', 'charttime']).reset_index(drop=True)
+# uo = uo.rename(columns={'value':'uo'})
 
 
-uo_criteria = pd.DataFrame()
-total_6hours = []
+# uo_criteria = pd.DataFrame()
+# total_6hours = []
 
-for idx, row in tqdm.tqdm(uo.iterrows()):
-# for uo_idx in tqdm.tqdm(range(0,10)):
-    pid = uo['pat_id'][idx]
-    ch_time = uo['charttime'][idx]
-    after_6hour = ch_time + timedelta(hours=6) 
+# for idx, row in tqdm.tqdm(uo.iterrows()):
+# # for uo_idx in tqdm.tqdm(range(0,10)):
+#     pid = uo['pat_id'][idx]
+#     ch_time = uo['charttime'][idx]
+#     after_6hour = ch_time + timedelta(hours=6) 
 
-    if re.sub(r'[^0-9]', '', uo['uo'][idx]) == '':
-        continue
+#     if re.sub(r'[^0-9]', '', uo['uo'][idx]) == '':
+#         continue
 
-    else:
-        uo_tmp = uo[(uo['pat_id']==pid) & (uo['charttime'] >= ch_time) & (uo['charttime'] < after_6hour)]
-        tmp_list = [re.sub(r'[^0-9]', '', x) for x in list(uo_tmp['uo'])]
-        tmp_list = [int(x) for x in tmp_list if x != '']
+#     else:
+#         uo_tmp = uo[(uo['pat_id']==pid) & (uo['charttime'] >= ch_time) & (uo['charttime'] < after_6hour)]
+#         tmp_list = [re.sub(r'[^0-9]', '', x) for x in list(uo_tmp['uo'])]
+#         tmp_list = [int(x) for x in tmp_list if x != '']
 
-        if sum(tmp_list) < 180:
-            uo_criteria = uo_criteria.append(uo.iloc[idx])
-            total_6hours.append(sum(tmp_list))
+#         if sum(tmp_list) < 180:
+#             uo_criteria = uo_criteria.append(uo.iloc[idx])
+#             total_6hours.append(sum(tmp_list))
 
-uo_criteria['total_6hours'] = total_6hours
-uo_criteria = uo_criteria.reset_index(drop=True)
-uo_criteria.to_csv(data_dir + 'uo_criteria.csv', encoding='cp949')
-print(uo_criteria)
+# uo_criteria['total_6hours'] = total_6hours
+# uo_criteria = uo_criteria.reset_index(drop=True)
+# uo_criteria.to_csv(data_dir + 'uo_criteria.csv', encoding='cp949')
+# print(uo_criteria)
+
+
+''' Diaylsis    '''
+
+# Dialysis
+dialysis = data_df[data_df['item']=='hemodialysis'].iloc[:, :-1].reset_index(drop=True)
+dialysis = dialysis.rename(columns={'value':'hemodialysis', 'charttime': 'starttime'})
+dialysis = dialysis.sort_values(['pat_id', 'starttime'])
+dialysis = dialysis.groupby('pat_id').head(1).reset_index(drop=True)
+print(dialysis)
+
+dialysis.to_csv(data_dir + 'dialysis.csv', encoding='cp949')
+
+# ICU 기간 내에 있는 경우만 포함 
+# filtered_dialysis = pd.DataFrame()
+
+# for idx in tqdm.tqdm(range(len(dialysis))):
+#     pid = dialysis['pat_id'][idx]
+#     ch_time = dialysis['starttime'][idx]
+#     # print('pid: ',  pid, 'charttime: ', ch_time)
+#     tmp = icuinfo_df[icuinfo_df['pat_id']==pid].reset_index(drop=True)   # icustays.csv 내에 존재하는 pat_id 데이터 전부 조회
+
+#     # 1개의 pat_id 에 여러 icu 입실기간이 있으므로, 모든 icu 입실기간의 intime & outtime 조회하여 이벤트 발생시각이 존재하는 경우 최종 라벨(filtered_result)에 추가함.
+#     for k in range(len(tmp)):
+#         intime = tmp['intime'][k]
+#         outtime = tmp['outtime'][k]
+
+#         if (ch_time >= intime) & (ch_time <= outtime):
+#             filtered_dialysis = filtered_dialysis.append(dialysis.iloc[idx])
+
+# filtered_dialysis = filtered_dialysis.sort_values(['pat_id', 'starttime'])
+# filtered_dialysis = filtered_dialysis.groupby('pat_id').head(1).reset_index(drop=True)
+# filtered_dialysis.to_csv(data_dir + 'dialysis.csv', encoding='cp949')
+# print(filtered_dialysis)
